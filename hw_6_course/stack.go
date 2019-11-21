17acba5f87922954a2f590c6063fbfe408fc68d1
gusarov2k <motoface@yandex.ru>
2019-11-21 06:14:43 +0200

    add new home

--- hw_6_course/stack.go
+++ hw_6_course/stack.go
@@ -1,3 +1,7 @@
+// Реализовать тип IntStack , который
+// содержит стэк целых чисел. У него
+// должны быть методы Push(i int) и
+// Pop() int
 package main
 
 import (
@@ -5,16 +9,27 @@ import (
 )
 
 type Stack struct {
- ...
+	Ids      []int
+	CountPop int
 }
 
+func (s *Stack) Push(val int) {
+	s.Ids = append(s.Ids, val)
+	s.CountPop++
+}
+
+func (s *Stack) Pop() int {
+	result := s.Ids[s.CountPop-1]
+	s.CountPop--
+	return result
+}
 
 func main() {
-   s := Stack{}
-   s.Push(10)
-   s.Push(20)
-   s.Push(30)
-   fmt.Printf("expected 30, got %d\n", s.Pop())
-   fmt.Printf("expected 20, got %d\n", s.Pop())
-   fmt.Printf("expected 10, got %d\n", s.Pop())
+	s := Stack{}
+	s.Push(10)
+	s.Push(20)
+	s.Push(30)
+	fmt.Printf("expected 30, got %d\n", s.Pop())
+	fmt.Printf("expected 20, got %d\n", s.Pop())
+	fmt.Printf("expected 10, got %d\n", s.Pop())
 }
